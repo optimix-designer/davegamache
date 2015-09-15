078a3de7603e1b64b79d50dd9eb830954cda845a
Optimix Designer <optimix.designer@gmail.com>
2015-09-15 12:19:59 +0300

    Fixing several typos in comments
    accelrate --> accelerate, decelrate -> decelerate

--- parallax/js/picasso.js
+++ parallax/js/picasso.js
@@ -41,11 +41,11 @@
               {
                 'selector'    : '.explosion-byline',
                 'translateY'  : '-25%',
-                'opacity'     : [0, 1.75] // hack to accelrate opacity speed
+                'opacity'     : [0, 1.75] // hack to accelerate opacity speed
               } , {
                 'selector'    : '#domExplosionList',
                 'translateY'  : '-70%',
-                'opacity'     : [0, 1] // hack to accelrate opacity speed
+                'opacity'     : [0, 1] // hack to accelerate opacity speed
               }
             ]
           } , {
@@ -62,18 +62,18 @@
                 'selector'    : '.dei-2',
                 'translateY'  : '-5%',
                 'translateX'  : '-4%',
-                'opacity'     : [1, 0] // hack to decelrate opacity speed
+                'opacity'     : [1, 0] // hack to decelerate opacity speed
               } , {
                 'selector'    : '.dei-3',
                 'translateY'  : '-9%',
                 'translateX'  : '2%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 1.2,
               } , {
                 'selector'    : '.dei-4',
                 'translateY'  : '-17%',
                 'translateX'  : '8%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 1.5,
               } , {
                 'selector'    : '.dei-5',
@@ -85,19 +85,19 @@
                 'selector'    : '.dei-6',
                 'translateY'  : '-1%',
                 'translateX'  : '-7%',
-                'opacity'     : [1, 0], // hack to decelrate opacity speed
+                'opacity'     : [1, 0], // hack to decelerate opacity speed
                 'scale'       : 1.2,
               } , {
                 'selector'    : '.dei-7',
                 'translateY'  : '-4%',
                 'translateX'  : '2%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 1.1,
               } , {
                 'selector'    : '.dei-8',
                 'translateY'  : '-3%',
                 'translateX'  : '12%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 1.8,
               } , {
                 'selector'    : '.dei-9',
@@ -109,18 +109,18 @@
                 'selector'    : '.dei-10',
                 'translateY'  : '5%',
                 'translateX'  : '-4%',
-                'opacity'     : [1, 0] // hack to decelrate opacity speed
+                'opacity'     : [1, 0] // hack to decelerate opacity speed
               } , {
                 'selector'    : '.dei-11',
                 'translateY'  : '8%',
                 'translateX'  : '6%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 1.4,
               } , {
                 'selector'    : '.dei-12',
                 'translateY'  : '1%',
                 'translateX'  : '20%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 1.9,
               } , {
                 'selector'    : '.dei-13',
@@ -132,19 +132,19 @@
                 'selector'    : '.dei-14',
                 'translateY'  : '4%',
                 'translateX'  : '-3%',
-                'opacity'     : [1, 0], // hack to decelrate opacity speed
+                'opacity'     : [1, 0], // hack to decelerate opacity speed
                 'scale'       : 1.3,
               } , {
                 'selector'    : '.dei-15',
                 'translateY'  : '14%',
                 'translateX'  : '5%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 1.7,
               } , {
                 'selector'    : '.dei-16',
                 'translateY'  : '6%',
                 'translateX'  : '9%',
-                'opacity'     : [1, 0], // hack to accelrate opacity speed
+                'opacity'     : [1, 0], // hack to accelerate opacity speed
                 'scale'       : 2,
               }
             ]
@@ -155,7 +155,7 @@
               {
                 'selector'    : '.explosion-byline',
                 'translateY'  : ['-25%', '-40%'],
-                'opacity'     : [1, 0] // hack to accelrate opacity speed
+                'opacity'     : [1, 0] // hack to accelerate opacity speed
               }
             ]
           } , {
@@ -165,7 +165,7 @@
               {
                 'selector'    : '.images-byline',
                 'translateY'  : '-25%',
-                'opacity'     : [0, 1.75] // hack to accelrate opacity speed
+                'opacity'     : [0, 1.75] // hack to accelerate opacity speed
               } , {
                 'selector'    : '#mediumHomepage',
                 'translateY'  : '-90%'
@@ -186,7 +186,7 @@
                 'selector'    : '.images-byline',
                 'translateY'  : ['-25%', '-25%'],
                 'scale'       : .7,
-                'opacity'     : [1.75, -.75] // hack to accelrate opacity speed
+                'opacity'     : [1.75, -.75] // hack to accelerate opacity speed
               } , {
                 'selector'    : '.images-byline-2',
                 'opacity'     : [0, 1],
